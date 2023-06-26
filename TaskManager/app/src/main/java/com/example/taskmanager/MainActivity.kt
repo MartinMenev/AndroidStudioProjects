5a202035a8c6b726968bc5c589c50ac96a1ea43e
Martin Menev <martin.menew@gmail.com>
2023-06-26 15:17:17 +0300

    Optimized imports, extracted main logic into a function, refactored the padding modifiers.

--- TaskManager/app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ TaskManager/app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,9 +20,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,10 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ImageGreeting(
-                        stringResource(R.string.task_message),
-                        stringResource(R.string.task_greeting)
-                    )
+                    TaskCompletedScreen()
                 }
             }
         }
@@ -47,8 +44,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun TaskCompletedScreen() {
+    ImageGreeting(
+        stringResource(R.string.task_message),
+        stringResource(R.string.task_greeting)
+    )
+}
+
+@Composable
 fun ImageGreeting(message: String, greeting: String, modifier: Modifier = Modifier) {
     Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -63,7 +71,7 @@ fun ImageGreeting(message: String, greeting: String, modifier: Modifier = Modifi
             lineHeight = 116.sp,
             fontWeight = FontWeight.Bold,
             color = Color.Black,
-            modifier = Modifier.padding(0.dp, 24.dp, 0.dp, 8.dp )
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp )
         )
         Text(
             text = greeting,
@@ -78,9 +86,6 @@ fun ImageGreeting(message: String, greeting: String, modifier: Modifier = Modifi
 @Composable
 fun GreetingPreview() {
     TaskManagerTheme {
-        ImageGreeting(
-            stringResource(R.string.task_message),
-            stringResource(R.string.task_greeting)
-        )
+        TaskCompletedScreen()
     }
 }
