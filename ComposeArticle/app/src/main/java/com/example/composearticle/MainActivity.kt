9e52da04dea65add1fb905805dbeaf473821f96d
Martin Menev <martin.menew@gmail.com>
2023-06-26 06:53:12 +0300

    Adding styles and optimized imports

--- ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -12,10 +14,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composearticle.ui.theme.ComposeArticleTheme
-import androidx.compose.foundation.Image
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,15 +53,18 @@ fun Article (
         Image()
         Text(
             text = title,
-            modifier = modifier
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp),
         )
         Text(
             text = paragraph1,
-            modifier = modifier
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
         )
         Text(
             text = paragraph2,
-            modifier = modifier
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
         )
     }
 }
@@ -66,9 +73,8 @@ fun Article (
 fun Image() {
     Image(
         painter = painterResource(R.drawable.bg_compose_background),
-        contentDescription = null,
-        contentScale = ContentScale.Crop,
-        alpha = 0.5F
+        contentDescription = "image",
+        contentScale = ContentScale.FillWidth,
     )
 }
 
